1425d4fae0d02c3503c4083dab7eed5555c037ad
LaurenRolan <lauren_rolan@yahoo.com.br>
2018-09-17 13:56:20 -0300

    Ex4 done

--- TP1/Ex4/table.js
+++ TP1/Ex4/table.js
@@ -9,9 +9,11 @@ function fillSquare() {
 		var tr = document.createElement("tr");
 		var th = document.createElement("th");
 		var td = document.createElement("td");
+		var num = document.createTextNode(i);
+		var sqr = document.createTextNode(j);
 		
-		td.appendChild(toString(j));
-		th.appendChild(toString(i));
+		td.appendChild(sqr);
+		th.appendChild(num);
 		tr.appendChild(th);
 		tr.appendChild(td);
 		tbody.appendChild(tr);
@@ -20,7 +22,45 @@ function fillSquare() {
 }
 
 function fillMult() {
+	var i;
+	var j;
+	var tbody = document.createElement("tbody");
+	var table = document.getElementById("mult");
 
+	for ( i = -1; i <= 10; i++ ) { //Rows
+		var tr = document.createElement("tr");
+		for ( j = -1; j <= 10; j++ ) { //Collumns
+			var iText = document.createTextNode(i);
+			var jText = document.createTextNode(j);
+			var multText = document.createTextNode(j * i);
+			if(i == -1)
+			{
+				var th = document.createElement("th");
+				if(j > -1)
+				{
+					th.appendChild(jText);
+				}
+				tr.appendChild(th);
+			}
+			else
+			{
+				if(j == -1)
+				{
+					var th = document.createElement("th");
+					th.appendChild(iText);
+					tr.appendChild(th);
+				}
+				else
+				{
+					var td = document.createElement("td");
+					td.appendChild(multText);
+					tr.appendChild(td);
+				}
+			}
+		}
+		tbody.appendChild(tr);
+	}
+	table.appendChild(tbody);
 }
 
 window.onload = function() {
